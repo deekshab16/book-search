dff45aa101f98a0430a83d7768c4eab98e068b78
deeksha-bhatnagar <dbhatnagar@katalystadvantage.com>
2022-04-16 17:53:56 -0400

    added inputs

--- src/App.js
+++
@@ -1,8 +0,0 @@
-
-function App() {
-  return (
-   <h1>Book Search</h1>
-  );
-}
-
-export default App;
---
+++ src/components/App/component.jsx
@@ -0,0 +1,13 @@
+import Form from "../Form";
+import "./style.css";
+
+const App = () => {
+  return (
+    <div className="Wrapper">
+      <h1>Book Search App </h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
